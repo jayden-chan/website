e719b4760b6712ab6a25988e47f796205a257368
Jayden Chan <jaydencn7@gmail.com>
2022-05-29 18:29:41 -0700

    JC: Create dist dir in case it doesn't exist

--- util.sh
+++ util.sh
@@ -3,6 +3,7 @@
 if [ "$1" = "render" ]; then
     echo "rendering website"
     rm -rf ./dist/*
+    mkdir -p dist
     cp -r src/* dist
     node --enable-source-maps --unhandled-rejections=strict gen.js dist
 fi
