3294bbe97d721400347322a88cb0988dbe2e4c31
Jayden Chan <jaydencn7@gmail.com>
2022-05-31 14:43:03 -0700

    JC: Deploy script + CNAME file

--- util.sh
+++ util.sh
@@ -5,5 +5,20 @@ if [ "$1" = "render" ]; then
     rm -rf ./dist/*
     mkdir -p dist
     cp -r src/* dist
-    node --enable-source-maps --unhandled-rejections=strict gen.js dist
+    node --unhandled-rejections=strict gen.js dist
+fi
+
+if [ "$1" = "deploy" ]; then
+    echo "rendering website"
+    mkdir -p /tmp/dist
+    cp -r src/* /tmp/dist
+    node --unhandled-rejections=strict gen.js /tmp/dist
+    git checkout pages
+    git pull
+    rm -rf ./*
+    mv /tmp/dist/* .
+    git commit -m "JC: Deploy to gh-pages branch"
+    git push
+    rm -rf /tmp/dist
+    git checkout master
 fi
