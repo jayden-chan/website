9fbe0de9850446fbe56a89ea4708ca833d1b8578
Jayden Chan <jaydencn7@gmail.com>
2022-05-31 14:49:50 -0700

    JC: HTML escape inputs

--- gen.js
+++ gen.js
@@ -1,6 +1,15 @@
 const { readFileSync, writeFileSync } = require("fs");
 const { join } = require("path");
 
+function e(text) {
+  return text
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
 const resume = JSON.parse(
   readFileSync("./content/resume.json", { encoding: "utf8" })
 );
@@ -25,9 +34,9 @@ const templateReplace = (path, replacements) => {
 
 const renderCheat = (cheat) => {
   return `<div class="cheat">
-      <h2>${cheat.title}</h2>
-      <p>${cheat.tags.map((t) => `#${t}`).join(", ")}</p>
-      <pre>${cheat.code}</pre>
+      <h2>${e(cheat.title)}</h2>
+      <p>${cheat.tags.map((t) => `#${e(t)}`).join(", ")}</p>
+      <pre>${e(cheat.code)}</pre>
     </div>`;
 };
 
@@ -38,18 +47,18 @@ const renderExp = (exp) => {
   }
   return `<div class="exp${exp.print ? "" : " donotprint"}">
                 <div class="exprow">
-                  <h2><a href="${exp.website}">${exp.company}</a></h2>
+                  <h2><a href="${e(exp.website)}">${e(exp.company)}</a></h2>
                   <h3>
-                    <i class="fa-solid fa-location-dot"></i>${exp.location}
+                    <i class="fa-solid fa-location-dot"></i>${e(exp.location)}
                   </h3>
                 </div>
                 <div class="exprow">
-                  <h4>${exp.title}</h4>
-                  <h4>${exp.time}</h4>
+                  <h4>${e(exp.title)}</h4>
+                  <h4>${e(exp.time)}</h4>
                 </div>
                 <div class="expdesc">
                   <ul>
-${exp.roles.map((r) => `${" ".repeat(20)}<li>${r}</li>`).join("\n")}
+${exp.roles.map((r) => `${" ".repeat(20)}<li>${e(r)}</li>`).join("\n")}
                   </ul>
                 </div>
               </div>`;
@@ -59,15 +68,15 @@ const renderProject = (proj) => {
   return `<div class="exp project${proj.print ? "" : " donotprint"}">
                 <div class="projrow">
                   <h2>
-                    <a href="https://github.com/jayden-chan/${proj.github}"
-                      >${proj.title} <i class="fa-brands fa-github"></i
+                    <a href="https://github.com/jayden-chan/${e(proj.github)}"
+                      >${e(proj.title)} <i class="fa-brands fa-github"></i
                     ></a>
                   </h2>
-                  <h4>${proj.time}</h4>
+                  <h4>${e(proj.time)}</h4>
                 </div>
-                <h4>${proj.stack}</h4>
+                <h4>${e(proj.stack)}</h4>
                 <ul>
-${proj.roles.map((r) => `${" ".repeat(18)}<li>${r}</li>`).join("\n")}
+${proj.roles.map((r) => `${" ".repeat(18)}<li>${e(r)}</li>`).join("\n")}
                 </ul>
               </div>`;
 };
@@ -75,10 +84,10 @@ ${proj.roles.map((r) => `${" ".repeat(18)}<li>${r}</li>`).join("\n")}
 const renderAward = (award) => {
   return `<div class="exp award">
                 <div class="projrow">
-                  <h2>${award.result}</h2>
-                  <h4>${award.time}</h4>
+                  <h2>${e(award.result)}</h2>
+                  <h4>${e(award.time)}</h4>
                 </div>
-                <h4>${award.desc}</h4>
+                <h4>${e(award.desc)}</h4>
               </div>`;
 };
 
@@ -93,16 +102,16 @@ templateReplace(join(renderDir, "resume", "index.html"), {
     .map((a) => renderAward(a))
     .join("\n              "),
   "{{ langs }}": resume.skills.lang
-    .map((l) => `<p>${l}</p>`)
+    .map((l) => `<p>${e(l)}</p>`)
     .join("\n" + " ".repeat(18)),
   "{{ tech }}": resume.skills.tech
-    .map((t) => `<p>${t}</p>`)
+    .map((t) => `<p>${e(t)}</p>`)
     .join("\n" + " ".repeat(18)),
   "{{ tools }}": resume.skills.tools
-    .map((t) => `<p>${t}</p>`)
+    .map((t) => `<p>${e(t)}</p>`)
     .join("\n" + " ".repeat(18)),
   "{{ interests }}": resume.interests
-    .map((i) => `<p>${i}</p>`)
+    .map((i) => `<p>${e(i)}</p>`)
     .join("\n" + " ".repeat(16)),
 });
 
